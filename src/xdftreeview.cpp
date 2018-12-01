gmcgarragh/xdfv

commit b794289065fcd47f7cf2566859731d76c02eaba6

Add right-click context menu on tabs.

src/xdftreeview.cpp
@@ -96,6 +96,26 @@ void XDFTreeView::load()
 
 
 
+void XDFTreeView::showContextMenu(const QPoint &point)
+{
+    QMenu menu(this);
+
+    QAction *copy_item_name_action = new QAction("Copy item name", this);
+    connect(copy_item_name_action, SIGNAL(triggered()), this, SLOT(copyItemName()));
+    menu.addAction(copy_item_name_action);
+
+    menu.addSeparator();
+
+    QAction *view_data_table_action = new QAction("View data table", this);
+    view_data_table_action->setEnabled(((XDFTreeViewItem *) currentItem())->hasDataTable());
+    connect(view_data_table_action, SIGNAL(triggered()), this, SLOT(showDataTable()));
+    menu.addAction(view_data_table_action);
+
+    menu.exec(mapToGlobal(point));
+}
+
+
+
 void XDFTreeView::copyItemName()
 {
     copyItemName((XDFTreeViewItem *) currentItem(), 0);
@@ -193,27 +213,6 @@ void XDFTreeView::collapseAll()
         collapseItem(*it);
         ++it;
     }
-}
-
-
-
-void XDFTreeView::prepareMenu(const QPoint &pos)
-{
-    QMenu menu(this);
-
-    QAction *copy_item_name_action = new QAction("Copy item name", this);
-    connect(copy_item_name_action, SIGNAL(triggered()), this, SLOT(copyItemName()));
-    menu.addAction(copy_item_name_action);
-
-    menu.addSeparator();
-
-    QAction *view_data_table_action = new QAction("View data table", this);
-    view_data_table_action->setEnabled(((XDFTreeViewItem *) currentItem())->hasDataTable());
-    connect(view_data_table_action, SIGNAL(triggered()), this, SLOT(showDataTable()));
-    menu.addAction(view_data_table_action);
-
-    QPoint pt(pos);
-    menu.exec(mapToGlobal(pos));
 }
 
 

src/xdftabtreeview.cpp
@@ -7,6 +7,8 @@
  *
  ******************************************************************************/
 
+#include <qmenu.h>
+
 #include "xdfv.h"
 #include "xdftabtreeview.h"
 #include "xdftreeview.h"
@@ -25,6 +27,9 @@ XDFTabTreeView::XDFTabTreeView(QWidget *parent)
     font_size = default_font_size;
 
     QObject::connect(this, SIGNAL(tabCloseRequested(int)), this, SLOT(closeTab(int)));
+
+    setContextMenuPolicy(Qt::CustomContextMenu);
+    QObject::connect(this, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(showContextMenu(const QPoint &)));
 }
 
 
@@ -32,6 +37,26 @@ XDFTabTreeView::XDFTabTreeView(QWidget *parent)
 XDFTabTreeView::~XDFTabTreeView()
 {
 
+}
+
+
+void XDFTabTreeView::showContextMenu(const QPoint &point)
+{
+    QMenu menu(this);
+
+    int index = tabBar()->tabAt(point);
+
+    if (index == currentIndex()) {
+        QAction *expand_all_action = new QAction("Expand all", this);
+        connect(expand_all_action, SIGNAL(triggered()), this, SLOT(expandAll()));
+        menu.addAction(expand_all_action);
+
+        QAction *collapse_all_action = new QAction("Collapse all", this);
+        connect(collapse_all_action, SIGNAL(triggered()), this, SLOT(collapseAll()));
+        menu.addAction(collapse_all_action);
+    }
+
+    menu.exec(mapToGlobal(point));
 }
 
 

src/xdftabtreeview.h
@@ -40,6 +40,8 @@ signals:
     void colorizedChanged(bool);
 
 public slots:
+    void showContextMenu(const QPoint &point);
+
     void copyItemName();
 
     void find(QString &name);

src/xdftreeview.h
@@ -44,6 +44,8 @@ public:
     virtual void load();
 
 public slots:
+    void showContextMenu(const QPoint &point);
+
     void copyItemName();
     void copyItemName(XDFTreeViewItem *item, int column);
 
@@ -55,7 +57,6 @@ public slots:
     void collapseAll();
     void colorizeAll(bool color);
 
-    void prepareMenu(const QPoint &pos);
     virtual void showDataTable();
     virtual void showDataTable(XDFTreeViewItem *item, int column);
 

commit 99f1285b49e6a69605af9934577491c27585887b

Bug fix: Commit b7942890 broke the tree view item context menu.

src/xdftreeview.cpp
@@ -63,7 +63,7 @@ XDFTreeView::XDFTreeView(const char *file_name_, XDFV::FileType file_type, QWidg
     setRootIsDecorated(true);
     setSortingEnabled(false);
     setContextMenuPolicy(Qt::CustomContextMenu);
-    QObject::connect(this, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(prepareMenu(const QPoint &)));
+    QObject::connect(this, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(showContextMenu(const QPoint &)));
 }
 
 